1c036580e0b10a7b8d00681fccfb373fa25d64ac
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2011-08-20 22:59:46 +0900

    Added comments field

--- snoopdiag.c
+++ snoopdiag.c
@@ -1,10 +1,36 @@
 /*
- * RFC 1761 Snoop Packet Capture File Format
- * 
- * snoop ファイルを読んで、表示（その他）する
- * プログラム。 
- * gcc snoopdiag.c -o snoopdiag -lnsl
-*/
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License (the "License").
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+/*
+ * Copyright (c) 1986, 2010, Oracle and/or its affiliates. All rights reserved.
+ */
+/*
+ * Copright (c) 2010  Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
+ * All rights reserved.
+ */   
+
+/*
+ * Tools that diagnose tcp/udp packets captured using Solaris's snoop(1M) command. 
+ *
+ * See RFC 1761 Snoop Packet Capture File Format
+ */
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
